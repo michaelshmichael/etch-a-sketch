5aaf91e9ccf4e1bd3f454e4a3aff0d7fc844387d
Michael Johnson <michael.johnson@nu.edu.kz>
2020-09-12 22:20:16 +0600

    hovering included, grid tidied up

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 const gridContainer = document.querySelector('#gridContainer');
+const gridAmount = 45;
+const input = document.querySelector('button')
 
-function genGrid(x) {
-    for (i = 0; i < x; i++) {
-       let row = document.createElement('div');
+function genGrid(gridAmount) {
+    for (i = 0; i < gridAmount; i++) {
+       const row = document.createElement('div');
        row.classList.add('row');
+       row.style.height = `${800/gridAmount}px`
        gridContainer.appendChild(row);
+        for (j = 0; j < gridAmount; j++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('square');
+            gridSquare.style.width = `${800/gridAmount}px`
+            row.appendChild(gridSquare);
+        } 
     }
-
-    let rows = document.querySelector(".row")
-
-    for (i = 0; i < x; i++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('square');
-        rows.appendChild(gridSquare);
-        console.log('added square')
-    }   
 }
 
-genGrid(4);
+genGrid(gridAmount);
 
+//Mouseover drawing effect
+const squares = Array.from(document.getElementsByClassName('square'))
+squares.forEach(function(square){
+    square.addEventListener('mouseover', function(){
+        square.classList.add('squareAltered')
+    })
+})
 
-
-// function genSquare(x) {
-//      for (i = 0; i < x; i++) {
-            
-//             const gridSquare = document.createElement('div');
-//             gridSquare.classList.add('square');
-//             row.appendChild(gridSquare);
-//             console.log('added square')
-//         }   
-// }
-
-// genSquare(4);
+//This is a mess with NodeLists, etc. deleting child nodes
+input.addEventListener('click', function(){
+    const gridAmount = prompt("How many sqaures do you want?")
+    const gridSquare = document.querySelectorAll('.square', '.squareAltered');
+    const row = document.querySelectorAll('row');
+    // //row.removeChild(gridSquare)
+    // const row = document.getElementsByClassName("row");
+    // while (row.firstChild) {
+    //     row.removeChild(row.firstChild);
+    // }
+    row.parentNode.removeChild(row);
+    genGrid(gridAmount);
+})
