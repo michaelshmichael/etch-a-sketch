37c64584ca58df7da3b66c1edd789534e5680830
Michael Johnson <michael.johnson@nu.edu.kz>
2020-09-13 15:30:38 +0600

    random colors and also refactored

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const gridContainer = document.querySelector('#gridContainer');
-const gridAmount = 45;
 const input = document.querySelector('button')
+const gridAmount = 18;
+
+// generate random RGB
+function genRandomColor(){
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+}
 
 // creates the grid
 function genGrid(gridAmount) {
@@ -16,13 +25,26 @@ function genGrid(gridAmount) {
             row.appendChild(gridSquare);
         } 
     }
+    // giving coloring functionality
+    let squares = Array.from(document.getElementsByClassName('square'))
+    squares.forEach(function(square){
+    square.addEventListener('mouseover', function(){
+        square.style.backgroundColor = `${genRandomColor()}` 
+    })
+})
 }
 genGrid(gridAmount);
 
 
 // reset and resize the grid when click button
 input.addEventListener('click', function(){
-    const gridAmount = prompt("How many sqaures do you want?");
+    let gridAmount = prompt("How many sqaures do you want? Please enter a value from 2 - 60.");
+    
+    // should make a better loop here to stop null entrance.
+    if(gridAmount === '') {
+        let gridAmount = prompt("How many sqaures do you want? Please enter a value from 2 - 60.");
+    };
+    
     // deleting all squares and rows (innerHTML is a bit hacky) 
     const row = Array.from(document.getElementsByClassName('row'));
     row.forEach(function(row){
@@ -31,21 +53,9 @@ input.addEventListener('click', function(){
     gridContainer.innerHTML = '';
     // regenerate with new number
     genGrid(gridAmount);
-    
-    // why does this need to be repeated here
-    squares = Array.from(document.getElementsByClassName('square'))
-        squares.forEach(function(square){
-            square.addEventListener('mouseover', function(){
-                square.classList.add('squareAltered')
-            })
-        })
-})
+});
 
 
-// mouseover drawing effect
-let squares = Array.from(document.getElementsByClassName('square'))
-squares.forEach(function(square){
-    square.addEventListener('mouseover', function(){
-        square.classList.add('squareAltered')
-    })
-})
+
+
+
